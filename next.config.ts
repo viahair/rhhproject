e2a6f9f7d7d954b8fef7a26ff8f3bf4d409e54c8
viahairsalon <viasalonhair@gmail.com>
2025-03-20 15:56:06 +0800

    #

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // 這是 React 嚴格模式的設置，視需要開啟或關閉
+  eslint: {
+    ignoreDuringBuilds: true, // 忽略 build 過程中的 ESLint 錯誤
+  },
+  // 其他配置選項可以加在這裡
 };
 
 export default nextConfig;
