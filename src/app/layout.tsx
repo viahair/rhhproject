f1767d1a5061ed074f24ed6efa612387ed9fa263
viahairsalon <viasalonhair@gmail.com>
2025-03-22 18:03:54 +0800

    title name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,16 +18,8 @@ export const metadata: Metadata = {
   openGraph: {
     title: "RHH 預約系統", // LINE 預覽的標題
     description: "快速預約，方便又簡單！", // LINE 預覽的描述
-    url: "https://你的網站.com", // 你的網站 URL
+    url: "https://rhhproject.vercel.app/", // 你的網站 URL
     siteName: "RHH", // 站點名稱
-    images: [
-      {
-        url: "https://你的網站.com/og-image.jpg", // 預覽的圖片
-        width: 1200,
-        height: 630,
-        alt: "RHH 預約封面圖",
-      },
-    ],
     type: "website",
   },
 };
@@ -43,8 +35,7 @@ export default function RootLayout({
         {/* 這裡的 meta 讓 LINE 可以正確讀取 */}
         <meta property="og:title" content="RHH 預約系統" />
         <meta property="og:description" content="快速預約，方便又簡單！" />
-        <meta property="og:image" content="https://你的網站.com/og-image.jpg" />
-        <meta property="og:url" content="https://你的網站.com" />
+        <meta property="og:url" content="https://rhhproject.vercel.app/" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
