9dcdc90a23af4e8c27b4cd7c8efb8f83e29d352b
viahairsalon <viasalonhair@gmail.com>
2025-03-22 21:03:24 +0800

    index16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,22 @@ import { useState } from "react";
 import Calendar from "react-calendar"; // 只匯入 Calendar
 import "react-calendar/dist/Calendar.css"; // 引入日曆樣式
 
-
-import { enUS } from "react-calendar/dist/locale/en-US"; 
-
-
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function Home() {
-
   const [value, onChange] = useState<Value>(new Date());
-
 
   return (
     <div className="container">
       <h1>預約表單</h1>
 
       <div>
-      <Calendar onChange={onChange} value={value} />
+        <Calendar
+          onChange={onChange}
+          value={value}
+          locale="en-US" // 設置為英文
+        />
       </div>
     </div>
   );
