cb8013fedd0cb346129a566f7aa8b4ff72e5d5aa
viahairsalon <viasalonhair@gmail.com>
2025-03-22 20:50:15 +0800

    index12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import { useState } from "react";
 import Calendar from "react-calendar"; // 只匯入 Calendar
-import "react-calendar/dist/Calendar.css"; // 引入日曆樣式
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function Home() {
-  const [selectedDate, setSelectedDate] = useState<Date | Date[] | null>(null); // 使用 Date 或 Date[] 類型
 
-  // 處理日曆日期變更
-  const handleDateChange = (value: Date | Date[]) => {
-    setSelectedDate(value); // 更新選擇的日期或範圍
-  };
+  const [value, onChange] = useState<Value>(new Date());
+
 
   return (
     <div className="container">
@@ -18,10 +17,7 @@ export default function Home() {
       {/* 月曆 */}
       <div>
         <h3>選擇日期</h3>
-        <Calendar
-          onChange={handleDateChange} // 當選擇日期時更新
-          value={selectedDate} // 顯示當前選擇的日期
-        />
+      <Calendar onChange={onChange} value={value} />
       </div>
     </div>
   );
